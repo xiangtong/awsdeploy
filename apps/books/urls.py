d8949368353d042c7014fece591fa64dcd2b9340
Jason.Qiu <qiuxiangtong@gmail.com>
2017-02-27 13:14:17 -0800

    delete all old files

--- apps/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-  url(r'^$', views.index, name='index'),
-  url(r'^add/?$',views.add, name='add'),
-  url(r'^create/(?P<id>\d*)/?$',views.create, name='create'),
-  url(r'^(?P<id>\d+)/?$',views.detail, name='detail'),
-  url(r'^(?P<id>\d+)/delete/?$',views.delete, name='delete'),
-]
